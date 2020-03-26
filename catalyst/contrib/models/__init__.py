0cf8a69cf45679930053d7bed51cef7ae8d55fab
Anatoly <anatolii.s.potapov@gmail.com>
2020-03-26 12:33:35 +0300

    Revert "Contrib/hydra (#721)"
    This reverts commit 2c1125b7b242fe9b38f07676b6b356e117ebc310.

--- catalyst/contrib/models/__init__.py
+++ catalyst/contrib/models/__init__.py
@@ -1,7 +1,6 @@
 # flake8: noqa
 
 from .functional import get_convolution_net, get_linear_net
-from .hydra import Hydra
 from .sequential import (
     _process_additional_params, ResidualWrapper, SequentialNet
 )
